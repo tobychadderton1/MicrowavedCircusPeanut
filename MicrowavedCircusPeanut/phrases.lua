e653b4a8d9ab742672e0d1b6612473151ade7b1b
tobychadderton1 <cjr.senpai@gmail.com>
2019-08-29 20:27:02 +0100

    Removed Unneeded Character

--- MicrowavedCircusPeanut/phrases.lua
+++ MicrowavedCircusPeanut/phrases.lua
@@ -4,7 +4,7 @@ nouns = {"Your mother", "Your hair", "Your nan", "Your dog", "Your foot",
 
 verbs = {"looks like", "is", "smells like", "eats like", "made love with",
 "ate", "defacated on", "murdered", "killed", "was", "is like", "sounds like",
-"sniffed", "touched", "stroked", "shot", "wants to sniff", "wants to be",}
+"sniffed", "touched", "stroked", "shot", "wants to sniff", "wants to be"}
 
 adjectives = {"an octopus", "a fat turd", "a dead rodent", "a fish",
 "a frisky cat", "a retard", "foot fungus", "your sister", "an idiot",
